a1a2d22c5aa6c14b8f90445e30d1148b5daa99e1
Zhao <z_zhao24@tss-hr420-dm09.fc.ca>
2018-11-16 11:51:46 -0500

    adding js code

--- js/master.js
+++ js/master.js
@@ -1,7 +1,56 @@
 (() => {
 
-    function getData() {
-        let targetURL = "./includes/connect.php?modelNo=R58";
+    
+
+mounted : function(){
+    console.log('view is ready to go on the page');
+
+    
+
+    document.querySelector('#F55').click();
+},
+
+    beforeUpdate : function() {
+            console.log('things are gonna change');
+        },
+        updated : function(){
+            console.log('things are different now');
+            // move the preloader out of the element and hide it
+            let preloader = document.querySelector('.preloader-wrapper');
+
+            setTimeout(functin(){
+              preloader.classList.add('.hidden');
+            document.body.appendChild(preloader);  
+
+            }, 1000); // 1000ms = 1 s
+            
+
+        },
+
+        methods: [
+            assPreloader(parentEl){
+                //load the preloader into the parent element and make it draw
+                let preloader = document.querySelector('.preloader-wrapper');
+
+                parentEl.appendChild(preloader);
+
+                let animItem = bodymovin.loadAnimation({
+                    wrapper : document.querySelector('.preloader-wrapper'),
+                    animType : 'svg',
+                    loop : true,
+                    path : 'data/search.json'
+
+                })
+
+
+            },
+        ]
+     getData(e) {
+
+            
+        this.addPreloader(document.querySelector('.modelInfo'));
+
+        let targetURL = "./includes/connect.php?modelNo=${e.currentTarget.id}`;
 
         fetch(targetURL) // go get data and bring it back! good doogy
         .then(res => res.json()) // turn the result into a plain js object 
